a28e6c969311fedcc0e5fc7cd7358adff9dbe90e
David Lougheed <david.lougheed@gmail.com>
2023-02-02 14:18:26 -0500

    fix: py3.8 compat type hint

--- fasta_checksum_utils/_file.py
+++ fasta_checksum_utils/_file.py
@@ -4,5 +4,5 @@ from typing import Tuple
 from .algorithms import ChecksumAlgorithm
 
 
-async def checksum_file(file: Path, algorithms: Tuple[ChecksumAlgorithm, ...]) -> tuple[str, ...]:
+async def checksum_file(file: Path, algorithms: Tuple[ChecksumAlgorithm, ...]) -> Tuple[str, ...]:
     return tuple(await asyncio.gather(*(a.checksum_file(file) for a in algorithms)))
