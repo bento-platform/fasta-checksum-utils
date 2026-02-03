892711aeb450e964a673a4671e7d7a59af41d57e
David Lougheed <david.lougheed@gmail.com>
2026-02-03 13:26:21 -0500

    refact: remove Union type hints + add mypy for dev

--- fasta_checksum_utils/_file.py
+++ fasta_checksum_utils/_file.py
@@ -1,8 +1,7 @@
 import asyncio
 from pathlib import Path
-from typing import Union
 from .algorithms import ChecksumAlgorithm
 
 
-async def checksum_file(file: Union[Path, str], algorithms: tuple[ChecksumAlgorithm, ...]) -> tuple[str, ...]:
+async def checksum_file(file: Path | str, algorithms: tuple[ChecksumAlgorithm, ...]) -> tuple[str, ...]:
     return tuple(await asyncio.gather(*(a.checksum_file(file) for a in algorithms)))
